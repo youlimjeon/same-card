31b3ded5ad4941e2d24902d490e74a402b1156aa
eluka22 <eluka22@gmail.com>
2021-06-20 09:06:51 +0900

    Add: main button in Header

--- src/components/Header/index.js
+++ src/components/Header/index.js
@@ -1,25 +1,40 @@
 import React from "react";
 
+import { useHistory } from "react-router-dom";
 import styled from "styled-components";
+
+import Button from "../Shared/Button";
 
 const Container = styled.header`
   width: 100%;
   height: 60px;
+  padding: 0rem 1rem;
   display: flex;
+  justify-content: space-between;
   align-items: center;
   background-color: #ffaa11;
+  box-sizing: border-box;
 `;
 
 const Logo = styled.h1`
   margin: 0;
-  padding: 0.5rem;
+  padding: 0;
   font-size: 3.2vmin;
 `;
 
 const Header = () => {
+  const history = useHistory();
+
   return (
     <Container>
       <Logo>학교 이름</Logo>
+      <Button
+        padding="0.5rem 1rem"
+        fontSize="1rem"
+        title="메인으로 가기"
+        color="#000000"
+        handleClick={() => history.push("/")}
+      />
     </Container>
   );
 };
