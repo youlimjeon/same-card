e27e29ba5f8359fd631bb34a83b83aa503f46ee9
eluka22 <eluka22@gmail.com>
2021-06-20 09:05:28 +0900

    Add: Teams and Board in Game

--- src/components/Game/index.js
+++ src/components/Game/index.js
@@ -1,8 +1,31 @@
 import React from "react";
 
+import { useLocation } from "react-router-dom";
+import styled from "styled-components";
+
+import Board from "./Board";
+import Teams from "./Teams";
+
+const Container = styled.section`
+  width: 100%;
+  height: calc(100vh - 60px);
+  padding: 2rem;
+  display: flex;
+  align-items: center;
+  justify-content: center;
+  background: #f1f0f0;
+  box-sizing: border-box;
+`;
+
 const Game = () => {
+  const location = useLocation();
+  const teams = location.state?.teams || "5";
+
   return (
-    <div>Gamea</div>
+    <Container>
+      <Teams numbers={teams} />
+      <Board />
+    </Container>
   );
 };
 
