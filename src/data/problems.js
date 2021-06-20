aea3ca136d8d8094aaaf14e9e8906ae5fef187ec
eluka22 <eluka22@gmail.com>
2021-06-20 09:07:51 +0900

    Add: problems

---
+++ src/data/problems.js
@@ -0,0 +1,98 @@
+import TestImage from "../assets/test.jpg";
+
+const data = [
+  {
+    "number": 1,
+    "title": "문제 1",
+    "content": "세살 버릇 여든까지 간다",
+    "images": [
+      {
+        "src": TestImage,
+        "width": "400px",
+        "height": "auto",
+        "description": "속담"
+      }
+    ],
+    "pair": 2,
+    "isCleared": false
+  },
+  {
+    "number": 2,
+    "title": "문제 2",
+    "content": "세살 버릇 여든까지 간다",
+    "pair": 1,
+    "isCleared": false
+  },
+  {
+    "number": 3,
+    "title": "문제 3",
+    "content": "세살 버릇 여든까지 간다",
+    "pair": 4,
+    "isCleared": false
+  },
+  {
+    "number": 4,
+    "title": "문제 4",
+    "content": "세살 버릇 여든까지 간다",
+    "pair": 3,
+    "isCleared": false
+  },
+  {
+    "number": 5,
+    "title": "문제 5",
+    "content": "5번 문제입니다.",
+    "pair": 6,
+    "isCleared": false
+  },
+  {
+    "number": 6,
+    "title": "문제 6",
+    "content": "세살 버릇 여든까지 간다",
+    "pair": 5,
+    "isCleared": false
+  },
+  {
+    "number": 7,
+    "title": "문제 7",
+    "content": "세살 버릇 여든까지 간다",
+    "pair": 8,
+    "isCleared": false
+  },
+  {
+    "number": 8,
+    "title": "문제 8",
+    "content": "세살 버릇 여든까지 간다",
+    "pair": 7,
+    "isCleared": false
+  },
+  {
+    "number": 9,
+    "title": "문제 9",
+    "content": "세살 버릇 여든까지 간다",
+    "pair": 10,
+    "isCleared": false
+  },
+  {
+    "number": 10,
+    "title": "문제 10",
+    "content": "세살 버릇 여든까지 간다",
+    "pair": 9,
+    "isCleared": false
+  },
+  {
+    "number": 11,
+    "title": "문제 11",
+    "content": "세살 버릇 여든까지 간다",
+    "pair": 12,
+    "isCleared": false
+  },
+  {
+    "number": 12,
+    "title": "문제 11",
+    "content": "세살 버릇 여든까지 간다",
+    "pair": 11,
+    "isCleared": false
+  }
+];
+
+export default data;
